272d1ca728af8d7386a48dcc501da1ae1688bf03
mahi1212 <mahi121.mr@gmail.com>
2022-12-29 00:29:05 +0600

    checkbox added

---
+++ src/pages/Survey/checkBox.js
@@ -0,0 +1,31 @@
+export const six = [
+  {
+    type: "a. Grammar (গ্রামার)",
+  },
+  {
+    type: "b. Writing (রাইটিং)",
+  },
+  {
+    type: "c. Speaking (স্পিকি ং)",
+  },
+  {
+    type: "d. Grammar (গ্রামার)",
+  },
+];
+export const eight = [
+  {
+    type: "a. Reading (রিডি ং)",
+  },
+  {
+    type: "b. Writing (রাইটিং)",
+  },
+  {
+    type: "c. Listening (লিসেনি ং)",
+  },
+  {
+    type: "d. Speaking (স্পিকি ং)",
+  },
+  {
+    type: "e. Grammar (গ্রামার)",
+  },
+];
